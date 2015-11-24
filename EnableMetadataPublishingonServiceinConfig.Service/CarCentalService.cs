333fa04d6ae92ef2cb7d44b753e98cac0039f291
aitipachong <laiqiang0010@163.com>
2015-11-24 16:42:11 +0800

    Enable Metadata Publishing on Service in Conifg.

---
+++ EnableMetadataPublishingonServiceinConfig.Service/CarCentalService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnableMetadataPublishingonServiceinConfig.Service
+{
+    [ServiceBehavior(InstanceContextMode=InstanceContextMode.PerCall)]
+    public class CarCentalService : ICarCentalService
+    {
+        public double CalculatePrice(DateTime pickupDate, DateTime returnDate, string pickupLocation, string vehiclePerference)
+        {
+            //call to some internal business logic
+            Random r = new Random(DateTime.Now.Millisecond);
+            return r.NextDouble() * 500;
+        }
+    }
+}
---
+++ EnableMetadataPublishingonServiceinConfig.Service/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnableMetadataPublishingonServiceinConfig.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(CarCentalService));
+            try
+            {
+                host.Open();
+                Console.WriteLine("The car rental service is up.");
+                Console.ReadLine();
+                host.Close();
+            }
+            catch (CommunicationException ex)
+            {
+                host.Abort();
+                Console.WriteLine(ex);
+            }
+            catch (TimeoutException ex)
+            {
+                host.Abort();
+                Console.WriteLine(ex);
+            }
+            catch (Exception ex)
+            {
+                host.Abort();
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
